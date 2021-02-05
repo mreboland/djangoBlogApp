67d9e2d7c28021ade9418d231fd54f0c9ee1aef4
Edward Boland <mr.eboland@gmail.com>
2021-02-05 17:14:08 -0500

    migrated db, added superuser

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Characteristics of a typical blog will have a title, author, and body. We will need to model this behaviour.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE
+    )
+    body = models.TextField()
+    
+    def __str__(self):
+        return self.title
