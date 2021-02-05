ef1b436671dd35bfcd17d710df3faaf8f92fc576
Edward Boland <mr.eboland@gmail.com>
2021-02-05 18:19:01 -0500

    added tests for our pages

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,52 @@
+# We import get_user_model to reference our active User and TestCase
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Post
+
+# We want to ensure that the Post model works as well as its str representation. We also want to test ListView and DetailView.
+
+class BlogTests(TestCase):
+    
+    def setUp(self):
+        # We create a test user
+        self.user = get_user_model().objects.create_user(
+            username="testuser",
+            email="test@email.com",
+            password="secret"
+        )
+        # We create a sample blog post to test 
+        self.post = Post.objects.create(
+            title="A good title",
+            body="Nice body content",
+            author=self.user,
+        )
+
+    # The next two tests then confirm that both it's string representation and content are correct.
+    def test_string_representation(self):
+        post = Post(title="A simple title")
+        self.assertEqual(str(post), post.title)
+        
+    def test_post_content(self):
+        self.assertEqual(f"{self.post.title}", "A good title")
+        self.assertEqual(f"{self.post.author}", "testuser")
+        self.assertEqual(f"{self.post.body}", "Nice body content")
+    
+    # We confirm that our homepage return a 200 http status code, contains our body text, and uses the correct home.html template.
+    def test_post_list_view(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Nice body content")
+        self.assertTemplateUsed(response, "home.html")
+    
+    # We test that our detail page works as expected and that an incorrect page returns a 404.
+    def test_post_detail_view(self):
+        response = self.client.get("/post/1/")
+        no_response = self.client.get("/post/100000")
+        self.assertEqual(response.status_code, 200)
+        # The below generates a 301 and not a 404, generating a failure. This contradicts the lesson which says it should be a 404.
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, "A good title")
+        self.assertTemplateUsed(response, "post_detail.html")
+    
--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ class BlogListView(ListView):
     model = Post
     template_name = "home.html"
 
+# We define the model we're using, Post, and the template we want it associated with, post_detail. DetailView will provide a context object we can use in our template called either 'object' or the lowercased name of our model, 'post'. DetailView also expects either a primary key or a slug passed to it as the identifier.
 class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
