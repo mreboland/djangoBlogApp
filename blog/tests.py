676b70984780b846015847c17778e6ce10e0a767
Edward Boland <mr.eboland@gmail.com>
2021-02-05 19:27:47 -0500

    added tests for post create, update, and delete

--- blog/tests.py
+++ blog/tests.py
@@ -1,5 +1,6 @@
 # We import get_user_model to reference our active User and TestCase
 from django.contrib.auth import get_user_model
+from django.http import response
 from django.test import TestCase
 from django.urls import reverse
 
@@ -50,3 +51,27 @@ class BlogTests(TestCase):
         self.assertContains(response, "A good title")
         self.assertTemplateUsed(response, "post_detail.html")
     
+
+    # To test create view we make a new response and then ensure that the response goes through(status code 200) and contains our new title and body text
+    def test_post_create_view(self):
+        response = self.client.post(reverse("post_new"), {
+            "title": "New title",
+            "body": "New text",
+            "author": self.user.id,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertContains(Post.objects.last().title, "New title")
+        self.assertEqual(Post.objects.last().body, "New text")
+        
+    # For update view we access the first post–which has a pk of 1 which is passed in as the only argument–and we confirm that it results in a 302 redirect
+    def test_post_update_view(self):
+        response = self.client.post(reverse("post_edit", args="1"), {
+            "title": "Updated title",
+            "body": "Updated text"
+        })
+        self.assertEqual(response.status_code, 302)
+
+    # We test our delete view by confirming that if we delete a post, the status code is 302, a redirect since the item no longer exists
+    def test_post_delete_view(self):
+        response = self.client.post(reverse("post_delete", args="1"))
+        self.assertEqual(response.status_code, 302)
